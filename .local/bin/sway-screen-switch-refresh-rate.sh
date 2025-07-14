2fed76948abd3c3b96bf3fa22e97d03290dba05e
bonswouar <contact@florianmessmer.com>
2025-07-14 11:25:17 +0200

    feat: sway screen switch refresh rate takes arg

--- .local/bin/sway-screen-switch-refresh-rate.sh
+++ .local/bin/sway-screen-switch-refresh-rate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ## sway > switch to next supported refresh rate
+##        or to the rate passed as argument
 
 notify="notify-send -u low sway -i $HOME/.local/share/image/dunst/screen.svg"
 
@@ -18,20 +19,26 @@ _switch_sway_output_rate() {
     return 0
 }
 
-# select next refresh rate
-passed_current=""
-while IFS= read -r rate; do
-    if echo $current_rate | grep $rate > /dev/null; then
-        passed_current="1"
-    else
-        if [[ $passed_current ]]; then
-            rate=$((rate/1000))
-            _switch_sway_output_rate "$rate"
-            exit
+if [ $# -eq 0 ]; then
+    # select next refresh rate
+    passed_current=""
+    while IFS= read -r rate; do
+        if echo $current_rate | grep $rate > /dev/null; then
+            passed_current="1"
+        else
+            if [[ $passed_current ]]; then
+                rate=$((rate/1000))
+                _switch_sway_output_rate "$rate"
+                exit 0
+            fi
         fi
-    fi
-done <<< "$rates"
+    done <<< "$rates"
 
-# if not found (i.e current rate is the last one), use first rate
-rate=$(("$(echo "$rates" | head -n 1)"/1000))
-_switch_sway_output_rate "$rate"
+    # if not found (i.e current rate is the last one), use first rate
+    rate=$(("$(echo "$rates" | head -n 1)"/1000))
+else
+    rate="$1"
+fi
+
+# switch to rate if different from current
+[[ "$1" == "$((current_rate/1000))" ]] && exit 0 || _switch_sway_output_rate "$rate"
